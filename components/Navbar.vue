37f5a1d7d062504a5c1db4ac9a16c9798eab2026
Steve Popoola <stevepop@gmail.com>
2023-01-03 04:38:34 +0000

    Initial project setup and README update

---
+++ components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <header class="bg-inherit">
+        <div id="nav" class="main-nav max-w-2xl mx-auto flex justify-between items-center p-4 md:p-0"></div>
+    </header>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+<section class="flex flex-col hero bg-blue-800 bg-cover bg-center bg-no-repeat">
+    <div class="h-screen flex flex-col justify-center items-center text-white text-center p-6 -mt-nav md:-mt-nav-md min-h-75">
+        <h1 class="mb-16 text-4xl md:text-5xl">Hello from Laravel UK</h1>
+        <h2 class="mb-4 text-3xl opacity-75 font-light md:text-3xl">The most exciting developer group in the United Kingdom</h2>
+        <h3 class="mb-4 text-3xl opacity-75 font-light md:text-3xl">New Site Coming Soon...</h3>
+    </div>
+</section>
+</template>
